193735ec0cccc8eb20ccac86c8fdeba72a22fc28
Tanish Tanmay Sahoo <121039428+tanmaytanish@users.noreply.github.com>
2025-07-26 22:13:56 +0530

    Initialized Backend and made ADD-DOCTOR & LOGIN

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import connectDB from './config/mongodb.js';
+import { connect } from 'mongoose';
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+// API Endpoints
+app.use('/api/admin',adminRouter)    //localhost:4000/api/admin/add-doctor
+
+app.get("/",(req,res)=>{
+    res.send("API is workinggggg")
+})
+
+app.listen(port,()=>console.log("Server started on",port))
